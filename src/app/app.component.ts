18efbe71520d62c89737c40b70c1c09d39517977
Daniel Vazquez <smashing_halo@hotmail.com>
2019-01-27 09:28:39 -0600

    Creating header, body and footer components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'my-app';
+  title = 'Primer componente';
+  name = 'Daniel';
+  lastName = 'VazSan';
 }
